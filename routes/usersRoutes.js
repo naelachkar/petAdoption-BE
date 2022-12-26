86261d6826d104e7bb88d9e5ad62f56225e5d124
Naël Achkar <naelachkar@hotmail.fr>
2022-12-26 18:36:09 +0200

    Adding users DB

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,6 +1,33 @@
-const express = require('express');
+const fs = require("fs");
+const express = require("express");
 
 const router = express.Router();
 
+router.post("/login", (req, res) => {
+  const user = req.body;
+  try {
+    const users = JSON.parse(fs.readFileSync("./database/UsersDataSet.json"));
+    const foundUser = users.find((item) => item.email === user.email);
+    console.log(foundUser);
+    if (foundUser) {
+      if (user.password === foundUser.password) {
+        const userInfo = {
+          firstName: foundUser.firstName,
+          lastName: foundUser.lastName,
+          phoneNumber: foundUser.phoneNumber,
+          email: foundUser.email,
+          bio: foundUser.bio,
+        };
+        res.send(userInfo);
+      } else {
+        res.status(500).send(false);
+      }
+    } else {
+      res.status(500).send(false);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const PORT = process.env.PORT || 8080;
 const petsRoutes = require("./routes/petsRoutes");
+const usersRoutes = require("./routes/usersRoutes");
 
 app.use(express.json());
 app.use(cors());
 
+app.post("/login", usersRoutes);
 app.get("/pets", petsRoutes);
 
 app.listen(PORT, () => {
