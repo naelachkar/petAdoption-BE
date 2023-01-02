1cd903f462f58e35d5cbfe5f1bae274437ebe7ae
Naël Achkar <naelachkar@hotmail.fr>
2023-01-02 16:14:09 +0200

    Creating 'isPasswordCorrect' middleware

---
+++ middlewares/isPasswordCorrect.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const Users = require("../models/usersModels");
+
+async function isPasswordCorrect(req, res, next) {
+  try {
+    const user = await Users.findOne({ email: req.body.email });
+    const isCorrect = await bcrypt.compare(req.body.password, user.password);
+    if (!isCorrect) {
+      res.status(400).send("Invalid password");
+      return;
+    } 
+    next();
+  } catch (err) {
+    res.status(500).send(err);
+  }
+}
+
+module.exports = isPasswordCorrect;
--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -2,31 +2,21 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 const Users = require("../models/usersModels");
-const doesUserExist = require("../middlewares/doesUserExist")
+const doesUserExist = require("../middlewares/doesUserExist");
+const isPasswordCorrect = require("../middlewares/isPasswordCorrect");
 
-router.post("/", doesUserExist, async (req, res) => {
-  const user = req.body;
+router.post("/", doesUserExist, isPasswordCorrect, async (req, res) => {
   try {
-    const users = await Users.find();
-    const foundUser = users.find((item) => item.email === user.email);
-    if (foundUser) {
-      const isPasswordCorrect = await bcrypt.compare(user.password, foundUser.password)
-      if (isPasswordCorrect) {
-        const userInfo = {
-          firstName: foundUser.firstName,
-          lastName: foundUser.lastName,
-          phoneNumber: foundUser.phoneNumber,
-          email: foundUser.email,
-          bio: foundUser.bio,
-        };
-        res.json(userInfo);
-      } else {
-        res.status(400).send("Invalid email or password");
-      }
-    } else {
-      res.status(500).send(false);
-    }
-  } catch (err) {
+    const user = await Users.findOne({ email: req.body.email });
+    const userInfo = {
+      firstName: user.firstName,
+      lastName: user.lastName,
+      phoneNumber: user.phoneNumber,
+      email: user.email,
+      bio: user.bio,
+    };
+    res.json(userInfo);
+  } catch (error) {
     res.status(500).send(err);
   }
 });
