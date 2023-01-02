cd39d5712e553da25c4bd3a7052f9a0db653d655
Naël Achkar <naelachkar@hotmail.fr>
2023-01-02 16:26:35 +0200

    Fixing password middleware

---
+++ controllers/login.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+const Users = require("../models/usersModels");
+
+router.post("/", async (req, res) => {
+  try {
+    const user = await Users.findOne({ email: req.body.email });
+    const userInfo = {
+      id: user._id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      phoneNumber: user.phoneNumber,
+      email: user.email,
+      bio: user.bio,
+      admin: user.admin,
+    };
+    res.json(userInfo);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+module.exports = router;
--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -1,24 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcryptjs");
-const Users = require("../models/usersModels");
 const doesUserExist = require("../middlewares/doesUserExist");
 const isPasswordCorrect = require("../middlewares/isPasswordCorrect");
+const login = require("../controllers/login")
 
-router.post("/", doesUserExist, isPasswordCorrect, async (req, res) => {
-  try {
-    const user = await Users.findOne({ email: req.body.email });
-    const userInfo = {
-      firstName: user.firstName,
-      lastName: user.lastName,
-      phoneNumber: user.phoneNumber,
-      email: user.email,
-      bio: user.bio,
-    };
-    res.json(userInfo);
-  } catch (error) {
-    res.status(500).send(err);
-  }
-});
+router.post("/", doesUserExist, isPasswordCorrect, login);
 
 module.exports = router;
