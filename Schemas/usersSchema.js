09f968c1425f8c182866773a777d9a3fc18890a7
Naël Achkar <naelachkar@hotmail.fr>
2023-01-10 16:26:43 +0200

    small fix with user schema

--- Schemas/usersSchema.js
+++ Schemas/usersSchema.js
@@ -1,10 +1,6 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  _id: {
-    type: mongoose.Types.ObjectId,
-    required: true,
-  },
   firstName: {
     type: String,
     required: true,
